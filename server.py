10749c0b4de9a081c8bfb39b92f32ed534797de7
SirWilliamIII <willcarpenterwastaken@gmail.com>
2025-02-09 11:10:24 -0600

    changed port

--- server.py
+++ server.py
@@ -64,5 +64,5 @@ def get_balance():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 4000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
