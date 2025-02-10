8c0db55d7f113e789cc35134e42c138d63395408
SirWilliamIII <willcarpenterwastaken@gmail.com>
2025-02-10 11:41:19 -0600

    added dockerfile

--- server.py
+++ server.py
@@ -130,5 +130,5 @@ def get_pow_attempts(index):
     return jsonify({"message": "Block not found"}), 404
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5005))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
