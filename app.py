b9c2e672d3bd0c859e42ae8c0c218485c21ea00d
SirWilliamIII <willcarpenterwastaken@gmail.com>
2025-02-10 23:49:39 -0600

    debug False

--- app.py
+++ app.py
@@ -230,4 +230,4 @@ if __name__ == "__main__":
     # Get port from environment variable or use 5000 as default
     port = int(os.environ.get("PORT", 5000))
     # Run the app with debug mode and allow all hosts
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
